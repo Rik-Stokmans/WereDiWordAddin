9f0aab6746693197d1c5659d8450c7ef7244409a
Rik-Stokmans <71502476+Rik-Stokmans@users.noreply.github.com>
2022-10-20 11:49:26 +0200

    added a time display

--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -9,11 +9,44 @@ Office.onReady((info) => {
   if (info.host === Office.HostType.Word) {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = run;
+    document.getElementById("HelloWorld").onclick = HelloWorld;
   }
 });
 
-export async function run() {
+var clock = document.getElementById('time');
+var endTime = document.getElementById('TimeSelector');
+
+function Clock() {
+  var time = new Date();
+  var hours = time.getHours().toString();
+  var minutes = time.getMinutes().toString();
+  var seconds = time.getSeconds().toString();
+
+
+  if (hours.length < 2) {
+    hours = '0' + hours;
+  }
+
+  if (minutes.length < 2) {
+    minutes = '0' + minutes;
+  }
+
+  if (seconds.length < 2) {
+    seconds = '0' + seconds;
+  }
+
+  var clockStr = hours + ' : ' + minutes + ' : ' + seconds;
+  endTime.setAttribute("min", (hours + ":" + minutes));
+  console.log(hours + ":" + minutes);
+
+  clock.textContent = clockStr;
+
+}
+
+Clock();
+setInterval(Clock, 1000);
+
+export async function HelloWorld() {
   return Word.run(async (context) => {
     /**
      * Insert your Word code here
