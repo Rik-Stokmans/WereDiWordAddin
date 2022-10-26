636d2baea4b758e896a4eb3cef98080809cac188
Rik-Stokmans <71502476+Rik-Stokmans@users.noreply.github.com>
2022-10-27 00:59:37 +0200

    remade the schedule system with a grid

--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -9,19 +9,17 @@ Office.onReady((info) => {
   if (info.host === Office.HostType.Word) {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
-    document.getElementById("HelloWorld").onclick = HelloWorld;
   }
 });
 
 var clock = document.getElementById('time');
-var endTime = document.getElementById('TimeSelector');
+var endTimeSelector = document.getElementById('timeSelector');
+
 
 function Clock() {
   var time = new Date();
   var hours = time.getHours().toString();
   var minutes = time.getMinutes().toString();
-  var seconds = time.getSeconds().toString();
-
 
   if (hours.length < 2) {
     hours = '0' + hours;
@@ -31,14 +29,20 @@ function Clock() {
     minutes = '0' + minutes;
   }
 
-  if (seconds.length < 2) {
-    seconds = '0' + seconds;
+  //sets the time to a variable
+  var clockStr = hours + ':' + minutes;
+
+  //checks if the timer is still on the same minute
+  const prevTime = clock.textContent.split(":");
+  if (prevTime[1] != (" "+minutes+" ")) {
+    //updates the minimum value for the end time
+    if (minutes == " 59 ") endTimeSelector.setAttribute("min", ((hours+1) + ":00"));
+    else {
+      
+    }
   }
 
-  var clockStr = hours + ' : ' + minutes + ' : ' + seconds;
-  endTime.setAttribute("min", (hours + ":" + minutes));
-  console.log(hours + ":" + minutes);
-
+  //updates the clock variable to the current time
   clock.textContent = clockStr;
 
 }
@@ -46,14 +50,14 @@ function Clock() {
 Clock();
 setInterval(Clock, 1000);
 
-export async function HelloWorld() {
+export async function HelloWorld(debug, debug2) {
   return Word.run(async (context) => {
     /**
      * Insert your Word code here
      */
 
     // insert a paragraph at the end of the document.
-    const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
+    const paragraph = context.document.body.insertParagraph(debug + " - " + debug2, Word.InsertLocation.end);
 
     // change the paragraph color to blue.
     paragraph.font.color = "blue";
